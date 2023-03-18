df19b7dfd46bcf7fbd02c68282284fbe01c23dc3
Jeremy new computer <jeremy8@vt.edu>
2023-03-18 17:32:57 -0400

    ok

--- src/Banner.js
+++ src/Banner.js
@@ -18,42 +18,42 @@ function Banner() {
   };
 
   return (
-    <div className="flex justify-evenly sticky top-0 z-50 h-14 lg:h-20 max-w-screen-2xl bg-opacity-60 bg-white rounded-lg border-b border-gray-600">
-      <div className="flex items-center h-full space-x-20  w-full lg:w-auto">
-        <div className="flex items-center space-x-10 bg-white rounded-lg px-10 py-2">
-          <h1 className="text-green-600 text-xl lg:text-4xl font-medium">Hobby Hunter</h1>
-          <img src={logo} alt="Hobby Hunter logo" className="w-20 h-10 lg:w-52 lg:h-14 ml-2 rounded-lg hidden lg:block" />
-        </div>
+  <div className="flex justify-evenly sticky top-0 z-50 h-14 lg:h-20 max-w-screen-2xl bg-opacity-60 bg-white rounded-lg border-b border-gray-600">
+    <div className="flex items-center h-full space-x-20  w-full lg:w-auto">
+      
+      <h1 className="text-green-600 items-center text-xl lg:text-4xl font-medium">Hobby Hunter</h1>
+      <img src={logo} alt="Hobby Hunter logo" className="w-20 h-10 lg:w-52 lg:h-14 ml-2 rounded-lg hidden lg:block" />
+      
 
-        {user ? (
-          <div className="flex items-center space-x-20">
-            <div className="flex">
-              <h2 className="text-green-600 text-base lg:text-3xl font-medium ml-2">
-                Welcome {user.displayName}!
-             </h2>
-             <Avatar src={user.photoUrl} className="ml-2 lg:ml-5 w-8 h-8 lg:w-10 lg:h-10" />
-            </div>
-            <div className="">
-              <button
-                className="px-3 py-2 text-white bg-green-600 rounded-lg font-medium hover:bg-red-600 transition duration-150 ease-in-out ml-2 lg:ml-5 text-sm lg:text-base"
-                onClick={logoutOfApp}
-              >
-                Logout
-              </button>
-            </div>
+      {user ? (
+        <div className="flex items-center space-x-20">
+          <div className="flex">
+            <h2 className="text-green-600 text-base lg:text-3xl font-medium ml-2">
+              Welcome {user.displayName}!
+            </h2>
+            <Avatar src={user.photoUrl} className="ml-2 lg:ml-5 w-8 h-8 lg:w-10 lg:h-10" />
           </div>
-        ) : (
-          <h1 className="text-green-600 text-base lg:text-4xl font-medium mr-5">
-            Find a new hobby today!
-          </h1>
-        )}
-
-        <div className="block lg:hidden">
-          <img src={logo} alt="Hobby Hunter logo" className="w-12 h-8 rounded-lg" />
+          <div className="">
+            <button
+              className="px-3 py-2 text-white bg-green-600 rounded-lg font-medium hover:bg-red-600 transition duration-150 ease-in-out ml-2 lg:ml-5 text-sm lg:text-base"
+              onClick={logoutOfApp}
+            >
+              Logout
+            </button>
+          </div>
         </div>
+      ) : (
+        <h1 className="text-green-600 text-base lg:text-4xl font-medium mr-5">
+          Find a new hobby today!
+        </h1>
+      )}
+
+      <div className="block lg:hidden">
+        <img src={logo} alt="Hobby Hunter logo" className="w-12 h-8 rounded-lg" />
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default Banner;
