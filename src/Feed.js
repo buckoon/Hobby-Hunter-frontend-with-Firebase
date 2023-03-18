120dac98c525f58e56cab59f1b88bd50d26cf7c8
Jeremy new computer <jeremy8@vt.edu>
2023-03-18 16:05:14 -0400

    Feed component is woorking

--- src/Feed.js
+++ src/Feed.js
@@ -67,63 +67,58 @@ function Feed() {
           Add a Hobby <AddIcon className="ml-2" size={25} />
         </button>
       </div>
-      <div className="flex p-5 mb-5 justify-center rounded-lg border border-gray-300 bg-white shadow-lg bg-opacity-50">
-        <div
-          className={
-            displayinput
-              ? "ease-in duration-300 w-full max-w-md bg-opacity-80 rounded-md p-8 z-10"
-              : <p></p>
-          }
-          style={{display:displayinput ? "block" : "none"}}
-        >
-          <form className="flex flex-col gap-4" onSubmit={sendHobbys}>
-            <div className="flex flex-col">
-              <label htmlFor="hobby" className="text-black font-bold mb-2">
-                List your favorite hobby:
-              </label>
-              <input
-                type="text"
-                id="hobby"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="instructions" className="text-black font-bold mb-2">
-                Instructions and supplies:
-               </label>
-             <textarea
-                id="instructions"
-                value={input2}
-               onChange={(e) => setInput2(e.target.value)}
-               rows={3}
-                onInput={(e) => {
-                  e.target.rows = Math.ceil(e.target.scrollHeight / 20);
-                }}
-               className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="photo" className="text-black font-bold mb-2">
-                Add a photo:
-              </label>
-              <input
-                type="text"
-                id="photo"
-                value={input3}
-                onChange={(e) => setInput3(e.target.value)}
-                className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-green-500 text-white font-bold py-2 hover:shadow-xl px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+      <div className={displayinput ? "text-black  flex p-5 mb-5 bg-opacity-40 justify-center rounded-lg border border-gray-300  bg-white shadow-lg  flex-col z-10 ease-in duration-500": "absolute top-0  left-[100%]  "}>
+        {displayinput && (
+          <div className="form-section">
+            <form className="flex flex-col gap-4" onSubmit={sendHobbys}>
+              <div className="flex flex-col">
+                <label htmlFor="hobby" className="text-black font-bold mb-2">
+                  List your favorite hobby:
+                </label>
+                <input
+                  type="text"
+                  id="hobby"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="instructions" className="text-black font-bold mb-2">
+                  Instructions and supplies:
+                </label>
+                <textarea
+                  id="instructions"
+                  value={input2}
+                  onChange={(e) => setInput2(e.target.value)}
+                  rows={3}
+                  onInput={(e) => {
+                    e.target.rows = Math.ceil(e.target.scrollHeight / 20);
+                  }}
+                  className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="photo" className="text-black font-bold mb-2">
+                  Add a photo:
+                </label>
+                <input
+                  type="text"
+                  id="photo"
+                  value={input3}
+                  onChange={(e) => setInput3(e.target.value)}
+                  className="border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:border-green-500"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-green-500 text-white font-bold py-2 hover:shadow-xl px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
       </div>
       {hobbys.map(({ id, data: { name, profpic, description, instructions, photo } }) => (
         <Hobby
