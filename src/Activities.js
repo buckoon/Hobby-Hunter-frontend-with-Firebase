7e67c049cf707338d16709a49f3b71ca2610c4f4
Jeremy new computer <jeremy8@vt.edu>
2023-04-01 16:49:15 -0400

    added another useEffect Hook to store data to local storage.

--- src/Activities.js
+++ src/Activities.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Activities(){
-    const [activity, setActivity] = useState(null);
+function Activities() {
+  const [activity, setActivity] = useState(null);
 
-    const handleButtonClick = () => {
-        fetch("http://www.boredapi.com/api/activity/")
-            .then(response => response.json())
-            .then(data => setActivity(data.activity))
-            .catch(error => console.error(error));
-    };
+  useEffect(() => {
+    const storedActivity = localStorage.getItem("activity");
+    if (storedActivity) {
+      setActivity(storedActivity);
+    }
+  }, []);
 
-    return (
-        <div>
-            <button 
-                className="bg-green-500 hover:shadow-xl text-white font-bold py-2 px-6 mb-2 rounded focus:outline-none focus:ring-2 focus:ring-green-600"
-                onClick={handleButtonClick}
-            >
-                Get Activity
-            </button>
-            {activity}
-        </div>
-    );
+  const handleButtonClick = () => {
+    fetch("http://www.boredapi.com/api/activity/")
+      .then((response) => response.json())
+      .then((data) => {
+        setActivity(data.activity);
+        localStorage.setItem("activity", data.activity);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <button
+        className="bg-green-500 hover:shadow-xl text-white font-bold py-2 px-6 mb-2 rounded focus:outline-none focus:ring-2 focus:ring-green-600"
+        onClick={handleButtonClick}
+      >
+        Get Activity
+      </button>
+      {activity}
+    </div>
+  );
 }
 
 export default Activities;
