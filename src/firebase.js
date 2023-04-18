5977b71b855bcb0b7e511048970afbc0c6145836
Jeremy new computer <jeremy8@vt.edu>
2023-04-18 13:01:09 -0400

    Edited API Key

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import {getStorage} from "firebase/storage";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "hobby-hunter-c0a22.firebaseapp.com",
+    projectId: "hobby-hunter-c0a22",
+    storageBucket: "hobby-hunter-c0a22.appspot.com",
+    messagingSenderId: "1024529469354",
+    appId: "1:1024529469354:web:55498e825342df5ac1e5d3"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
+
+  export const storage = getStorage(firebaseApp);
